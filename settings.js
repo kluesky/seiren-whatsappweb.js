855274de117a32e2a40a9f2fd1831aa8b77769a6
kluesky <140289431+kluesky@users.noreply.github.com>
2025-07-16 14:28:18 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,11 @@
+const settings = {
+  owner: '6285606389689@c.us', // nomor WhatsApp kamu
+  prefix: '.',                 // prefix perintah bot
+  sessionName: 'session',      // nama folder sesi WA
+  timezone: 'Asia/Jakarta',    // zona waktu
+  botName: 'Seiren Bot',       // nama bot
+  version: '0.0.1',            // versi bot
+  watermark: 'Lyora',          // watermark untuk fitur stiker
+};
+
+export default settings;
